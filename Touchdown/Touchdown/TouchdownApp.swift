ee9cb9221e1b0a1fd03e6aee20ca3e7ae68d2d7a
LeoMe <eleomerelo@gmail.com>
2024-06-10 14:04:40 +0200

    Initial commit

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Leo Merelo on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
